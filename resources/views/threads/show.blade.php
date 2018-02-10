0b11653f5a7e0964b40121dc5e559fc75c7d78a7
sizukutamago <sizukutamago@gmail.com>
2018-02-10 14:48:29 +0900

    05 https://laracasts.com/series/lets-build-a-forum-with-laravel/episodes/5

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,25 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="post" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="body">Body:</label>
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign</a> in to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
