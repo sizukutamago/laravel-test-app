46b9fb258dfe312a4ef2ceb3475335e4e7f423cf
sizukutamago <sizukutamago@gmail.com>
2018-02-10 15:30:56 +0900

    06 https://laracasts.com/series/lets-build-a-forum-with-laravel/episodes/6

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 認証されていないユーザは新しくスレッドを作成出来ない()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    public function 認証されたユーザは新しくスレッドを作成出来る()
+    {
+        $this->actingAs(factory('App\User')->create());
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
